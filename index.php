c9d6e011348cfb8d618758f5dcdff4f98f3a4cdf
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2022-05-13 19:51:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,129 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-  <meta charset="utf-8">
-  <title>Inicio</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
-
-  <!-- Favicons -->
-  <link href="" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
-
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Persona</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
+    <?php
+    include 'db.php';
+    $conexiondb = conectardb();
+    $query = "SELECT * FROM paises ORDER BY nombre ASC";
+    $resultado = mysqli_query($conexiondb, $query);
+    mysqli_close($conexiondb);
+    ?>
 
-  <!-- Static navbar -->
-  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        <a class="navbar-brand" href="index.php">UNIVERSIDAD CATOLICA</a>
-      </div>
-      <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li class="active"><a href="index.php">Formulario</a></li>
-          <li><a href="materia.php" class="smoothscroll">Materia</a></li>  
-          <li><a href="listado.php" class="smoothscroll">Listado</a></li>
-          <li><a href="generar.php" class="smoothscroll">Generar PDF</a></li>
-        </ul>
-      </div>
-      <!--/.nav-collapse -->
+    <h1 id="titulo_principal" class="text-center">Registrar Persona</h1>
+
+    <div class="col-sm-4 offset-sm-4">
+        <br>
+        <form action="guardar_persona.php" method="post">
+
+            <!--<input type="hidden" name="">-->
+
+
+            <div class="mb-3">
+                <th><b><label for="">Cedula:</label></b></th>
+                <th><input class="form-control" type="text" name="cedula" id=""></th>
+            </div>
+
+
+            <div class="mb-3">
+                <th><b><label for="">Nombre:</label></b></th>
+                <th><input class="form-control" type="text" name="nombre" id=""></th>
+            </div>
+
+            <div class="mb-3">
+                <th><b><label for="">Apellido:</label></b></th>
+                <th><input class="form-control" type="text" name="apellido" id=""></th>
+            </div>
+
+            <div class="mb-3">
+                <th><b><label for="">Pais:</label></b></th>
+                <th>
+                    <select name="id_pais" class=" form-control" id="inputGroupSelect01">
+                        <?php
+                        while ($pais = mysqli_fetch_assoc($resultado)) {
+                            echo "<option value='".$pais['id']."'>".$pais['nombre']."</option>";
+                        }
+                        ?>
+                    </select>
+                </th>
+            </div>
+            <input type="hidden" name="editar" id="" value='no' readonly>
+            <input class="btn btn-outline-primary" type="submit" value="GUARDAR">
+        </form>
     </div>
-  </div>
 
-  <div id="contactwrap">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 col-lg-offset-3">
-          <h4>BIENVENIDO</h4>
-          <h1>UNIVERSIDAD CATOLICA</h1>
-        </div>
-      </div>
-    </div>
-    <!-- /container -->
-  </div>
-
-  <div id="contact">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <h2 class="centered">REGISTRO DEL DOCENTE</h2>
-
-          <form class="contact-form php-mail-form" role="form" action="guardar_docente.php" method="POST">
-          <div class="form-group">
-              <input type="text" name="cedula" class="form-control" id="contact-subject" placeholder="Cedula" data-rule="minlen:4" data-msg="Ingrese su numero de cedula">
-              <div class="validate"></div>
-            </div>
-            <div class="form-group">
-              <input type="name" name="nombre" class="form-control" id="contact-name" placeholder="nombre" data-rule="minlen:4" data-msg="Por favor, complete los datos" >
-              <div class="validate"></div>
-            </div>
-            <div class="form-group">
-              <input type="name" name="apellido" class="form-control" id="contact-name" placeholder="apellido" data-rule="minlen:4" data-msg="Por favor, complete los datos" >
-              <div class="validate"></div>
-            </div>
-            <div class="form-group">
-              <input type="name" name="fecha_nacimiento" class="form-control" id="contact-name" placeholder="fecha_nacimiento" data-rule="minlen:4" data-msg="Por favor, complete los datos" >
-              <div class="validate"></div>
-            </div>
-            <div class="form-group">
-              <input type="name" name="profesion" class="form-control" id="contact-name" placeholder="profesion" data-rule="minlen:4" data-msg="Por favor, complete los datos" >
-              <div class="validate"></div>
-            </div>
-            <div class="error-message">error</div>
-            <div class="sent-message">Formulario completado</div>
-            <br>
-            <input type="hidden" name="editar" value="no" readonly>
-            <div class="form-send">
-              <button type="submit" class="btn btn-large">Enviar</button>
-            </div>
-
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-
-  <div id="copyrights">
-    <div class="container">
-      <div class="credits">
-
-        TODOS LOS DERECHOS RESERVADOS <a href="https://templatemag.com/">PROGRAMACION II</a>
-      </div>
-    </div>
-  </div>
-  <!-- / copyrights -->
-
-  <!-- JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-  <script src="lib/php-mail-form/validate.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="js/main.js"></script>
-
+    <link rel="stylesheet" href="./css/index.css">
 </body>
 </html>
